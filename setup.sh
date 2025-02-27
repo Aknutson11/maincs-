210bd5c48a722e5c8ea6f3075f9f47db7869a829
aknutson11 <andrew.knutson@icloud.com>
2025-02-26 19:03:23 -0800

    updated dockerfiles for ssh-server and client container

--- setup.sh
+++ setup.sh
@@ -1,10 +1,24 @@
 #create docker network(s)
+#TODO: determine correct docker networks to use
 
 #build Dockerfile for c1 (client)
+cd ./client
+docker build -t ssh-client:latest
+cd ..
 
 #build Dockerfile for f1 (flask)
+cd ./flask
+docker build -t flask-app:latest .
+cd ..
 
-#build Docerfile for s1 (ssh-server)
+#build Dockerfile for s1 (ssh-server)
+cd ./ssh-server
+docker build -t ssh-server:latest . 
+cd ..
 
 #build Dockerfile for r1 (routers)
 
+#run containers
+docker run -d --name myflaskapp -p 5000:5000 flask-app:latest
+docker run -d --name sshserver -p 2222:22 ssh-server:latest
+docker run -d --name client ssh-client:latest
